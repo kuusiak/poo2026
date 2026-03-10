763ef45a6c94480605be9e37518f938fa1b156c4
unknown <20241PVAI0030004@estudantes.ifpr.edu.br>
2026-03-10 09:54:44 -0300

    Aula 10/03 Não Terminada

--- caixaEletronica10.py
+++ caixaEletronica10.py
@@ -1,15 +1,25 @@
 saldo = 1000.00
 escolha = 0
 
-def saque(x):
+def saque(saldo, valorSaque):
+    if (valorSaque <= saldo):
+        saldo = saldo - valorSaque
+    elif (valorSaque > saldo):
+        print("⚠️ O valor do saque desejado é maior que o saldo atual.")
+    elif (saldo == 0):
+        print("Saldo Zerado")
+    return saldo
 
-    return x
+def deposito(saldo, valorDeposito):
+    saldo = saldo + valorDeposito
+    return saldo
 
 while (escolha != 4):
     escolha = int(input("1 - Sacar\n2 - Depositar\n3 - Extrato\n4 - Sair"))
     if (escolha == 1):
         print(f"Saldo atual: {saldo}")
-        valorSaque = float(input("Informe o valor que valor você deseja sacar: "))
-        if (valorSaque <= saldo):
-            x = saque(valorSaque)
-        
+        valorSaque = input("Informe o valor que valor você deseja sacar: ")
+        saldo = saque(saldo, valorSaque)
+    elif (escolha == 2):
+        valorDeposito = input("Informe o valor que você deseja depositar.")
+        saldo = deposito(saldo, valorDeposito)
