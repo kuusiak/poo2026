0afdcd143e7415564842d8e8a99b541faeef88cf
kuusiak <20241PVAI0030004@estudantes.ifpr.edu.br>
2026-04-27 19:27:04 -0300

    Teste

--- Trabalho POO 28,04/principal.py
+++ Trabalho POO 28,04/principal.py
@@ -1,4 +1,9 @@
-opcao = 8
+from classes import Jogador, Equipe
+
+jogadores = []
+equipes = [] 
+
+opcao = 7
 while (opcao != 0):
     texto = """========================================
 CAMPEONATO INTERCLASSE DE E-SPORTS
@@ -15,16 +20,58 @@ CAMPEONATO INTERCLASSE DE E-SPORTS
     opcao = int(input("Escolha uma opção: "))
 
     if(opcao == 1):
-        w
+        nome = input("Digite o nome do jogador: ")
+        turma = input("Digite a turma: ")
+        nickname = input("Digite o nickname: ")
+
+        jogador = Jogador(nome, turma, nickname)
+        jogadores.append(jogador)
+
+        print("Jogador cadastrado com sucesso!")
+
     elif(opcao == 2):
-        w
+        nome = input("Digite o nome da equipe: ")
+        jogo = input("Digite o jogo que a equipe irá jogar: ")
+
+        equipe = Equipe(nome, jogo)
+        equipes.append(equipe)
+
+        print("Equipe cadastrada com sucesso!")
+
     elif(opcao == 3):
-        w
+        if len(jogadores) == 0:
+            print("Nenhum jogador cadastrado.")
+
+        elif len(equipes) == 0:
+            print("Nenhuma equipe cadastrada.")
+
+        else:
+            print("Jogadores cadastrados:")
+            for i in range(len(jogadores)):
+                print(f"{i + 1}. ", end="")
+            jogadores[i].exibir_dados()
+
+            escolha_jogador = int(input("Escolha o número do jogador: ")) - 1
+
+            print("Equipes cadastradas:")
+            for i in range(len(equipes)):
+                print(f"{i + 1}. {equipes[i].nome_equipe} ({equipes[i].jogo})")
+
+            escolha_equipe = int(input("Escolha o número da equipe: ")) - 1
+
+            jogador = jogadores[escolha_jogador]
+            equipe = equipes[escolha_equipe]
+            equipe.adicionar_jogadores(jogador)
+
+            print("Jogador adicionado à equipe com sucesso!")
     elif(opcao == 4):
         w
+
     elif(opcao == 5):
         w
+
     elif(opcao == 6):
         w
+
     else:
-        print("Escolha uma opçãi válida!")
+        print("Escolha uma opção válida!")
