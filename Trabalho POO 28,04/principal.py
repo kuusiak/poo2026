a0ce365a968a14cd6ab99840139873cfa960022d
kuusiak <20241PVAI0030004@estudantes.ifpr.edu.br>
2026-04-27 21:13:07 -0300

    Trabalho de POO (Finalizado)

--- Trabalho POO 28,04/principal.py
+++ Trabalho POO 28,04/principal.py
@@ -3,9 +3,7 @@ from classes import Jogador, Equipe
 jogadores = []
 equipes = [] 
 
-opcao = 7
-while (opcao != 0):
-    texto = """========================================
+texto = """========================================
 CAMPEONATO INTERCLASSE DE E-SPORTS
 ========================================
 1. Cadastrar jogador
@@ -16,10 +14,14 @@ CAMPEONATO INTERCLASSE DE E-SPORTS
 6. Buscar jogador por nickname
 0. Sair
 ========================================"""
-    print(texto)
+print(texto)
+
+opcao = 7
+while (opcao != 0):
     opcao = int(input("Escolha uma opção: "))
 
     if(opcao == 1):
+        print("--- Cadastrar Jogador ---")
         nome = input("Digite o nome do jogador: ")
         turma = input("Digite a turma: ")
         nickname = input("Digite o nickname: ")
@@ -30,6 +32,7 @@ CAMPEONATO INTERCLASSE DE E-SPORTS
         print("Jogador cadastrado com sucesso!")
 
     elif(opcao == 2):
+        print("--- Cadastrar Equipe ---")
         nome = input("Digite o nome da equipe: ")
         jogo = input("Digite o jogo que a equipe irá jogar: ")
 
@@ -39,6 +42,7 @@ CAMPEONATO INTERCLASSE DE E-SPORTS
         print("Equipe cadastrada com sucesso!")
 
     elif(opcao == 3):
+        print("--- Adicionar Jogador a uma Equipe ---")
         if len(jogadores) == 0:
             print("Nenhum jogador cadastrado.")
 
@@ -49,7 +53,7 @@ CAMPEONATO INTERCLASSE DE E-SPORTS
             print("Jogadores cadastrados:")
             for i in range(len(jogadores)):
                 print(f"{i + 1}. ", end="")
-            jogadores[i].exibir_dados()
+                jogadores[i].exibir_dados()
 
             escolha_jogador = int(input("Escolha o número do jogador: ")) - 1
 
@@ -65,13 +69,42 @@ CAMPEONATO INTERCLASSE DE E-SPORTS
 
             print("Jogador adicionado à equipe com sucesso!")
     elif(opcao == 4):
-        w
+        print("Equipes cadastradas:")
+        if len(equipes) == 0:
+            print("Não existem equipes cadastradas.")
+        else:
+            for i in range(len(equipes)):
+                print(f"\n[{i + 1}]", end=" ")
+                equipes[i].exibir_info()
 
     elif(opcao == 5):
-        w
-
+        print("--- Jogadores da Equipe ---")
+        if len(equipes) == 0:
+            print("Não existem equipes cadastradas.")
+        else:
+            print("Equipes cadastradas:")
+            for i in range(len(equipes)):
+                print(f"{i + 1}. {equipes[i].nome_equipe} ({equipes[i].jogo})")
+            escolha_equipe = int(input("Escolha o número da equipe: ")) - 1
+            print(f"Jogadores da equipe {equipes[escolha_equipe].nome_equipe}")
+            equipes[escolha_equipe].listar_jogadores()
+        
     elif(opcao == 6):
-        w
+        print("--- Busca de Jogador ---")
+        if len(jogadores) == 0:
+            print("Nenhum jogador cadastrado no sistema.")
+        nickname_escolhido = input("Informe o nickname que deseja buscar: ")
+        encontrado = 0
+        for jogador in jogadores:
+            if jogador.nickname == nickname_escolhido: 
+                jogador.exibir_dados()
+                encontrado = 1
+                break
+        if encontrado == 0:
+            print("Não existe um jogador com esse nickname cadastrado.")
+
+    elif(opcao == 0):
+        print("Encerrando o sistema.")
 
     else:
         print("Escolha uma opção válida!")
