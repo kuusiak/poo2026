44e1a2acf20ecb34592298950ad2c81b3bdaff37
unknown <20241PVAI0030004@estudantes.ifpr.edu.br>
2026-06-02 09:53:40 -0300

    Adicionando movimento e um sprite novo

--- JOGOS/meu-jogo.py
+++ JOGOS/meu-jogo.py
@@ -11,22 +11,47 @@ class Player(arcade.Sprite):
         self.textura_esquerda = arcade.load_texture("player_esquerda.png")
         super().__init__("player_direita.png", scale=0.5)
     def uptade(self, delta_time):
-        pass
+        self.center_x += self.center_x
+        self.center_y += self.center_y
+        if (self.change_x > 0):
+            self.texture = self.textura_direita
+        elif (self.change_x < 0):
+            self.texture = self.textura_esquerda
+
+class Moeda(arcade.Sprite):
+    def __init__ (self):
+        super().__init__("ghost.png", scale=0.7)
+    def uptade(self, delta_time):
+        self.center_x += self.center_x
+        self.center_y += self.center_y
 
 class JanelaJogo(arcade.Window):
     def __init__(self):
         super().__init__(LARGURA, ALTURA, TITULO)
         arcade.set_background_color(arcade.color.BUBBLE_GUM)
-        self.personagem = Player()
-        self.personagem.center_x = 400
-        self.personagem.center_y = 300
+        self.movimento = 3
+        self.jogador = Player()
+        self.jogador.center_x = 400
+        self.jogador.center_y = 300
+        self.jogador.change_x = self.movimento
+        self.sprite_jogador = arcade.SpriteList()
+        self.sprite_jogador.append(self.jogador)
+        self.moeda = Moeda()
+        self.moeda.center_x = 600
+        self.moeda.center_y = 300
+        self.moeda.change_x = self.movimento
+        self.moeda.change_y = self.movimento
+        self.sprite_moedas = arcade.SpriteList()
+        self.sprite_moedas.append(self.moeda)
     
     def on_draw(self):
         self.clear()
-        arcade.draw_sprite(self.personagem)
+        self.sprite_jogador.draw()
+        self.sprite_moedas.draw()
 
     def on_update(self, delta_time):
-        pass
+        self.sprite_jogador.update(delta_time)
+        self.sprite_moedas.update(delta_time)
 
 def main():
     tela = JanelaJogo()
